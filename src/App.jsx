d99bebf8b62f7a744ed6dd75f9c3215bbb447814
khurramdeveloper55 <shehzadkhurram744@gmail.com>
2025-11-24 10:47:21 +0500

    first ui commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="pt-[100px] pb-[75px]">
+        <span className="text-xs font-semibold tracking-[.3em] uppercase open-sans primary-yellow">
+          Shop by style
+        </span>
+        <h3>Timeless favorites loved by watch</h3>
+      </section>
+      <h1 className="text-neutral-500">Hello From React</h1>
+      <p className="text-blue text-4xl font-bold">How are You</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

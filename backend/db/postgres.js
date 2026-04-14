24e324659ec84bb1c69136bf45413c94fd1ad56b
khurramdeveloper55 <shehzadkhurram744@gmail.com>
2026-04-14 22:44:03 +0500

    ev updated

--- backend/db/postgres.js
+++ backend/db/postgres.js
@@ -1,35 +1,40 @@
 import dotenv from "dotenv";
 import path from "path";
+import { Sequelize } from "sequelize";
+
 dotenv.config({
   path: path.join(process.cwd(), "config.env"),
 });
-import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_USER_PASSWORD,
-  {
-    host: process.env.DATABASE_HOST,
-    dialect: process.env.DATABASE_DIALECT,
-    logging: false,
-    dialectOptions: {
-      ssl:
-        process.env.NODE_ENV === "production"
-          ? {
-              require: true,
-              rejectUnauthorized: false,
-            }
-          : false,
-    },
-  },
-);
+const sequelize =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize(process.env.DATABASE_URL, {
+        dialect: "postgres",
+        logging: false,
+        dialectOptions: {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false,
+          },
+        },
+      })
+    : new Sequelize(
+        process.env.DATABASE_NAME,
+        process.env.DATABASE_USER,
+        process.env.DATABASE_USER_PASSWORD,
+        {
+          host: process.env.DATABASE_HOST,
+          dialect: process.env.DATABASE_DIALECT,
+          logging: false,
+        },
+      );
 
 let UserModel = null;
+
 const connection = async () => {
   try {
     await sequelize.authenticate();
-    console.log("Connection has been established succesfully.");
+    console.log("Connection has been established successfully.");
   } catch (error) {
     console.log("Unable to connect to database", error);
   }
